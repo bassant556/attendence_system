2604db4e75e896960f1740f259ea4c0cceb39bf3
bassant556 <101362400+bassant556@users.noreply.github.com>
2023-05-06 23:13:30 +0300

    Add files via upload

---
+++ attendence monitoring system/src/attendence/monitoring/system/AttendenceMonitoringSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package attendence.monitoring.system;
+
+/**
+ *
+ * @author bassant
+ */
+public class AttendenceMonitoringSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        login l1=new login();
+         new login().setVisible(true);
+        
+    }
+    
+}
